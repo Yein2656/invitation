8ea649a34b9be69025cd6f2b57a49442bd4b93bf
yein2656 <lyi0223@daum.net>
2024-12-23 21:19:52 +0900

    fix: 컨텐츠 수정

--- src/main/java/com/project/invitation/controller/InvitationController.java
+++ src/main/java/com/project/invitation/controller/InvitationController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InvitationController {
 
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("data","hello");
+        return "invitation";
+    }
     @GetMapping("invitation")
     public String invitation(Model model) {
         model.addAttribute("data","hello");
